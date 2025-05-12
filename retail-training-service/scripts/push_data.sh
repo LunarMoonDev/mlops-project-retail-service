25185634855e92ac99353b500cea1313b605af15
Harjit Brar <workandresearchbrar@gmail.com>
2025-05-12 11:31:37 +0800

    feat: training, add script for pushing data

---
+++ retail-training-service/scripts/push_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# constants
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+LOCAL_CSV="$SCRIPT_DIR/../.local/Fashion_Retail_Sales.csv"
+TODAY_STR=$(date +%F)
+S3_BUCKET="s3://data/train/${TODAY_STR}_batch_data.csv"
+
+# specify the aws profile to use
+export AWS_PROFILE=minio
+
+echo "Uploading csv to aws s3..."
+aws s3 cp $LOCAL_CSV $S3_BUCKET
