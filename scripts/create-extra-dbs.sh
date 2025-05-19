78eec8e7fd92fdd2bad3bf27d2214006adc576a5
Harjit Brar <workandresearchbrar@gmail.com>
2025-05-19 11:28:41 +0800

    refactor: add sql to create table for batch predictions

--- scripts/create-extra-dbs.sh
+++ scripts/create-extra-dbs.sh
@@ -3,3 +3,16 @@
 set -e
 psql -U sample -d postgres -c "create database training_prefect_db"
 psql -U sample -d postgres -c "create database batch_prefect_db"
+psql -U sample -d postgres -c "create database retail_db"
+psql -U sample -d retail_db -c "
+    CREATE TABLE IF NOT EXISTS retail_reviews (
+        order_id TEXT PRIMARY KEY,
+        customer_id TEXT NOT NULL,
+        item_purchased TEXT NOT NULL,
+        purchase_amount FLOAT NOT NULL,
+        date_purchase DATE NOT NULL,
+        review_rating FLOAT NOT NULL,
+        payment_method TEXT NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)
+"
